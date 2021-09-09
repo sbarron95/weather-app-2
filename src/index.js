5b1bc2d490ddf8a57b4a8d7f9a163afaab8061fe
Steph <stephbarron95@gmail.com>
2021-09-09 14:10:48 -0400

    Added src

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import WeatherSearch from "./WeatherSearch";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <WeatherSearch />
+      <footer>
+        {" "}
+        Coded by Stephanie Barron using React, open-sourced on GitHub, and
+        hosted on Netlify
+      </footer>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
+
+reportWebVitals();
